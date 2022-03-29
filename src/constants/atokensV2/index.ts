dc4e3bd8300667fe59db65a89ab5fc21e92bcd3f
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2022-03-29 19:17:16 +0300

    remove bad tokens

--- src/constants/atokensV2/index.ts
+++ src/constants/atokensV2/index.ts
@@ -1,12 +1,10 @@
 import polygon from './polygon'
-import arbitrum from './arbitrum'
 import avalanche from './avalanche'
 import mainnet from './mainnet'
 import { createNetworkList } from '../../utils'
 
 export default createNetworkList({
   polygon,
-  arbitrum,
   avalanche,
   mainnet
 })
