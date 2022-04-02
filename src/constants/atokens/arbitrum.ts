6e19e7b6fa34559aa29fd75e4b0850fc14bd74e0
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2022-04-02 18:09:29 +0300

    cleanup

--- src/constants/atokens/arbitrum.ts
+++
@@ -1,3 +0,0 @@
-import { createTokenUtils } from '../../utils'
-
-export default createTokenUtils([])
--- src/constants/ctokens/index.ts
+++ src/constants/ctokens/index.ts
@@ -1,10 +1,6 @@
-import polygon from './polygon'
-import arbitrum from './arbitrum'
 import mainnet from './mainnet'
 import { createNetworkList } from '../../utils'
 
 export default createNetworkList({
-  polygon,
-  arbitrum,
   mainnet
 })
