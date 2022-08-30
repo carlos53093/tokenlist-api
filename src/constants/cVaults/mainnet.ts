ecbf7e076f2547c8dba2b1ec3bd5e0e7e46fccee
0xPradyuman <63545809+pradyuman-verma@users.noreply.github.com>
2022-08-30 15:38:07 +0530

    Update src/constants/cVaults/mainnet.ts
    Co-authored-by: Thrilok kumar <thrilok2000@gmail.com>

--- src/constants/cVaults/mainnet.ts
+++ src/constants/cVaults/mainnet.ts
@@ -1,6 +1,6 @@
 export default [
   {
-    vaultAddress: "0xc3d688B66703497DAA19211EEdff47f25384cdc3",
+    marketAddress: "0xc3d688B66703497DAA19211EEdff47f25384cdc3",
     baseAsset: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     vaultName: "USDC",
   },
