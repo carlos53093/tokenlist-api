fd01fd371acab0abcce4b6c2ddf65aab9a739f50
Thrilok kumar <thrilok2000@gmail.com>
2023-03-23 04:49:22 +0530

    Update src/constants/tokens/arbitrum.ts

--- src/constants/tokens/arbitrum.ts
+++ src/constants/tokens/arbitrum.ts
@@ -105,7 +105,7 @@ export default createTokenUtils([
     type: "token",
     symbol: "ARB",
     name: "Arbitrum",
-    address: "0x912ce59144191c1204e64559fe8253a0e49e6548",
+    address: "0x912CE59144191C1204E64559FE8253a0e49E6548",
     decimals: 18,
     isStableCoin: false,
   }
