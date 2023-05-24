82e7ac45216073cfabbb851c2f219d242972a9fc
q1q0 <carloslin9999@gmail.com>
2023-05-24 13:03:22 -0400

    add arbitrum compound v3

--- src/constants/cMarketsV3/index.ts
+++ src/constants/cMarketsV3/index.ts
@@ -1,8 +1,10 @@
 import { createNetworkList } from "../../utils";
 import mainnet from "./mainnet";
 import polygon from "./polygon";
+import arbitrum from "./arbitrum";
 
 export default createNetworkList({
   mainnet,
   polygon,
+  arbitrum,
 });
