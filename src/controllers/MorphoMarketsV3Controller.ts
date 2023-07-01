df885922c8675e74d6204fc92e20014ae6368b2c
Shriya Tyagi <shriya243@gmail.com>
2023-07-01 16:31:59 +0800

    update markets controller

--- src/controllers/MorphoMarketsV3Controller.ts
+++ src/controllers/MorphoMarketsV3Controller.ts
@@ -3,12 +3,12 @@ import morphoMarkets from "../constants/morpho-v3-markets";
 
 @JsonController()
 export class MorphoV3Controller {
-  @Get("/morpho/mainnet/v3/markets", { transformResponse: false })
+  @Get("/morpho/aave/v3/markets", { transformResponse: false })
   public index() {
     return morphoMarkets;
   }
 
-  @Get("/:network/morpho/mainnet/v3/markets", { transformResponse: false })
+  @Get("/:network/morpho/aave/v3/markets", { transformResponse: false })
   public byNetwork(@Param("network") network: string) {
     return morphoMarkets[network] || [];
   }
